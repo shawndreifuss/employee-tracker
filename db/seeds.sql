624fafed520bcd1cb3b179491185151043b45f70
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-14 16:45:45 -0700

    added startup prompt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO department (department_name)
 VALUES
+  ('HR'),
   ('Tech'),
   ('Marketing'),
   ('Finance'),
@@ -9,21 +10,21 @@ VALUES
   
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Manager ', 2000000, 1),
-  ('Marketer', 3000000, 3),
-  ('Software Engineer', 40000000, 2),
-  ('Attorney', 2000000, 7),
-  ('Salesperson', 1300000, 5),
-  ('Engineer', 1500000, 6),
-  ('Accountant', 1600000, 7),
-  ('Sales Lead', 1600053450, 5),
-  ('CEO', 400345000, 5);
+  ('Recruiter ', 20000, 1),
+  ('Marketer', 30000, 3),
+  ('Software Engineer', 40000, 2),
+  ('Attorney', 200000, 7),
+  ('Salesperson', 130000, 5),
+  ('Engineer', 150000, 6),
+  ('Accountant', 160000, 7),
+  ('Sales Lead', 160000, 5),
+  ('CEO', 400000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ron', 'bank', 1, 1),
-  ('Shawn', 'Wolf', 2, 2),
-  ('Jordan', 'Chill', 3, 1),
+  ('Jordan', 'Chill', 1, 1),
+  ('Shawn', 'Dreifusss', 2, 2),
+  ('Ryan', 'Brent', 3, 1),
   ('Octavia', 'Butler', 4, 3),
   ('John', 'Doe', 5, 1),
   ('Tom', 'Allen', 6, 3);
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express')
 
-const db = require('./config/connection')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,6 +10,64 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'shawn020496',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the emplyees_db database.`)
+  );
 
+  //promopts user at start up
+  function startUp() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee'], 
 
+    }).then( answer => {
+        switch (answer.menu) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update An Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete Department':
+                deleteDepartment();
+                break;
+            case 'Delete Role':
+                deleteRole();
+                break;
+            case 'Delete Employee':
+                deleteEmployee();
+                break;
+        }
+    })
+ };
 
+startUp()
