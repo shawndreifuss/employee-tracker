aeddb278edb20dc882e8fc771982678b9b15bba6
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-14 16:05:12 -0700

    added require and middleware

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const express = require('express')
+
+const db = require('./config/connection')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
 
 
 
